f2769a51aa4f99d8265e91c196250ca0f76e9f5f
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-23 09:47:56 -0600

    wip

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,9 +1,9 @@
 @isset($href)
-    <a {{ $attributes }} class="block px-4 py-3 text-left font-semibold transition hover:bg-neutral-100">
+    <a {{ $attributes }} class="block w-full px-4 py-3 text-left font-semibold transition hover:bg-neutral-100">
         {{ $slot }}
     </a>
 @else
-    <button {{ $attributes }} class="block px-4 py-3 text-left font-semibold transition hover:bg-neutral-100">
+    <button {{ $attributes }} class="block w-full px-4 py-3 text-left font-semibold transition hover:bg-neutral-100">
         {{ $slot }}
     </button>
 @endif
