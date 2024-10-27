b50a88f2a4a2a8b4f17b88753de60113f1b01a35
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-26 19:49:53 -0600

    Project setup (#1)
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<div class="fixed z-10 w-full bg-white shadow-sm">
+    <div class="border-b-[1px] py-4">
+        <x-container>
+            <div class="flex flex-row items-center justify-between gap-3 md:gap-0">
+                <x-logo />
+            </div>
+        </x-container>
+    </div>
+</div>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+use function Laravel\Folio\name;
+
+name('home') ?>
+
+<x-layouts.app>
+    <x-container>Index</x-container>
+</x-layouts.app>
