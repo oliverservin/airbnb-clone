2195bf4180c7dac98184bf70b5cffc0578de0e88
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-27 19:37:57 -0600

    wip

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,19 +8,6 @@
         @vite('resources/css/app.css')
     </head>
     <body class="h-full antialiased">
-        <div x-data="{ showRegisterModal: false }" x-on:show-register-modal.window="showRegisterModal = true">
-            <x-modal x-model="showRegisterModal">
-                <x-slot name="title">
-                    Registro
-                </x-slot>
-                <form action="">
-                    Formulario
-                </form>
-                <x-slot name="footer">
-                    <button>Crear cuenta</button>
-                </x-slot>
-            </x-modal>
-        </div>
         <x-navbar />
         <div class="pb-20 pt-28">{{ $slot }}</div>
     </body>
