c0da169e5a11c9133701f9ccfd040733a8e7f8bf
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-29 19:02:31 -0600

    wip

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -8,11 +8,11 @@
     x-cloak
     {{ $attributes->class('fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-neutral-800/70 outline-none focus:outline-none') }}
 >
-    <div class="relative mx-auto my-6 h-full w-full md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-2/5">
+    <div class="relative mx-auto h-full w-full md:w-4/6 lg:w-3/6 xl:w-2/5">
         <div x-transition class="translate h-full duration-300">
             <div
                 @click.away="showModal = false"
-                class="translate relative flex h-full w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none md:h-auto lg:h-auto"
+                class="translate relative my-6 flex h-full w-full flex-col overflow-y-scroll rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none md:h-auto lg:h-auto"
             >
                 <div class="relative flex items-center justify-center rounded-t border-b-[1px] p-6">
                     <button
