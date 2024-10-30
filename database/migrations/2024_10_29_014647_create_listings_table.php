ef42f5446b646fa4c84789cdd065f3cc97bc081e
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-30 14:22:59 -0600

    wip

--- database/migrations/2024_10_29_014647_create_listings_table.php
+++ database/migrations/2024_10_29_014647_create_listings_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('description');
             $table->integer('price');
+            $table->string('photo_path')->nullable();
             $table->timestamps();
         });
     }
