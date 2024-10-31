916d3f314cffc30950cc849f35de76e0e84fafc0
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2024-10-30 20:54:33 -0600

    Update listing-card.blade.php

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -3,7 +3,7 @@
 <div {{ $attributes }} class="group relative col-span-1">
     <div class="flex w-full flex-col gap-2">
         <div class="relative aspect-square w-full overflow-hidden rounded-xl bg-neutral-200">
-            <!-- Listing pho -->
+            <!-- Listing photo -->
             <!-- <img
                 fill
                 class="h-full w-full object-cover transition group-hover:scale-110"
